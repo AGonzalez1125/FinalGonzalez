7306ce49e319ce84f941c11987304432fe185cde
AGonzalez1125 <andre.g1125@icloud.com>
2024-11-30 23:53:10 -0500

    Update
    Project build

--- R/DataDownload.R
+++ R/DataDownload.R
@@ -3,9 +3,7 @@
 #
 # This will install the necessary packages and download the data from steam.
 
-hello <- function() {
-  print("Hello, world!")
-}
+
 
 install.packages("devtools")
 install.packages("roxygen2")
@@ -25,3 +23,50 @@ api_key <- readLines(file.choose())
 
 # Define the Steam API base URL
 steam_url <- "https://api.steampowered.com"
+
+## Call Function to create vector of valid steam IDs
+playerSteamID <- steamIDGenerate( api_key, n = 1000)
+
+
+
+## Use Steam ID to pull player information
+get_recent_games <- function(api_key, steam_id) {
+  # Construct the full API request URL
+  gameSummaryUrl <- paste0(steam_url, "/IPlayerService/GetRecentlyPlayedGames/v1/?key=", api_key, "&steamid=", steam_id
+  )
+
+  # Make the API request
+  games_response <- httr::GET(gameSummaryUrl)
+
+  if (httr::status_code(games_response) == 200) {
+    # Parse the JSON response
+    games_data <- jsonlite::fromJSON(httr::content(games_response, "text", encoding = "UTF-8"))
+    print("Parsed JSON:")
+    print(games_data)
+
+    # Check if 'games' field exists
+    if (!is.null(games_data$response$games)) {
+      return(games_data$response$games)
+    } else {
+      warning("No recently played games found for this Steam ID.")
+      return(NULL)
+    }
+  } else {
+    warning("Failed to fetch recently played games. Status code: ", httr::status_code(games_response))
+    return(NULL)
+  }
+}
+
+playerSummary1 <- get_recent_games(api_key, playerSteamID[10])
+
+print(playerSummary)
+
+PlayerGameSummmary <- function(playerSteamID){
+
+
+
+
+}
+
+
+
---
+++ R/SteamID.R
@@ -0,0 +1,52 @@
+## This file will generate random steam IDs to build out to use in the functions
+## to pull data
+## This function will also verify if the steam ID is valid using the api connection
+
+# Load required libraries
+library(httr)
+library(jsonlite)
+
+
+## Main function to create vector of steam id, starting with 10
+steamIDGenerate <- function(api_key,n) {
+  ## Steam ID are 17 digit numbers with in a range, define this range
+  lower_steam <- 76561197960265728
+  upper_steam <- 76561199999999999
+
+
+  ## Initialize steam vector of valid steam
+  verified_steam <- c()
+
+
+  ## Creating loop to fill vector
+  while (length(verified_steam) < n){
+  ## Marking here as i want to create a vector of steam ID, this will be used to
+  ## create the data frame of information
+
+    random_steam <- sample(seq(lower_steam, upper_steam), n - length(verified_steam), replace = TRUE)
+
+     ## Creating loop to pull steam id
+    for (steam_id in random_steam){
+
+      url <- paste0(steam_url, "/ISteamUser/GetPlayerSummaries/v2/?key=", api_key, "&steamids=", steam_id)
+
+      ## Jasonlite request to api
+      response <- httr::GET(url)
+
+      if (httr::status_code(response) == 200){
+        ## Parse
+        data <- jsonlite::fromJSON((httr::content(response, "text", encoding = "UTF-8")))
+
+      ## Check if player exists
+      if (length(data$response$players) > 0 ){
+          verified_steam <- c(verified_steam, steam_id)
+        }
+      }
+
+      ## Break the loop after enough valid steam IDs have been collected
+      if (length(verified_steam) >= n) break
+    }
+
+  }
+  return(verified_steam)
+}
